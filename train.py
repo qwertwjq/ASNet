919b460cea3dc7ac0fef129656a8541521a5a5eb
qwertwjq <97027063+qwertwjq@users.noreply.github.com>
2024-07-27 16:39:07 +0800

    Update train.py

--- train.py
+++ train.py
@@ -6,10 +6,7 @@ import os
 import numpy as np
 from time import time
 import os
-def standardization(data):
-    mu = np.mean(data, axis=0)
-    sigma = np.std(data, axis=0)
-    return (data - mu) / sigma
+
 BATCH_SIZE = 50
 
 def get_data():
